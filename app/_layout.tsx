048059683738cb6d26b7dd0542b06951974b9720
Seunghyun Min <i@seunghyun.in>
2026-02-19 16:28:49 +0900

    Disable scaling on mobile web

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Helmet } from 'react-helmet';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,6 +15,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Helmet>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
